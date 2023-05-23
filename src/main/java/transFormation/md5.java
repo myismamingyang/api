faa09bec7d990f81ac7d43ccda025175cedc9fc4
myismamingyang <126977818+myismamingyang@users.noreply.github.com>
2023-05-23 16:32:37 +0800

    md5 加密代码提交

---
+++ src/main/java/transFormation/md5.java
@@ -0,0 +1,22 @@
+package transFormation;
+
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class md5 {
+    public static String toMD5(String StringData){
+        byte[] secreByte = null;
+        try{
+            secreByte = MessageDigest.getInstance("md5").digest(StringData.getBytes());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("!!! md5算法错误 !!!");
+        }
+        String md5Code = new BigInteger(1, secreByte).toString(16);
+        for (int i = 0; i < 32 -md5Code.length(); i++) {
+            md5Code = "0" + md5Code;
+        }
+        return md5Code;
+    }
+}
