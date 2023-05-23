ea00e345808308e5a700b899099b645a6746f356
myismamingyang <126977818+myismamingyang@users.noreply.github.com>
2023-05-23 17:21:18 +0800

    md5 测试代码提交

---
+++ src/test/java/test.java
@@ -0,0 +1,6 @@
+public class test {
+    public static void main(String[] args) {
+        String s = md5.toMD5("FDMS20230419ZGDX00004009202304140000003590.pdf");
+        System.out.println(s);
+    }
+}
