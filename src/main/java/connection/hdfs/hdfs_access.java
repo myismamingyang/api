ec48f4f1df3f203195ac9feba3b35baf3203c9f1
myismamingyang <126977818+myismamingyang@users.noreply.github.com>
2023-05-29 14:04:04 +0800

    1

--- src/main/java/connection/hdfs/hdfs_access.java
+++ src/main/java/connection/hdfs/hdfs_access.java
@@ -1,4 +1,25 @@
 package connection.hdfs;
+import  org.apache.hadoop.conf.Configuration;
+import  org.apache.hadoop.fs.FileSystem;
+import  org.apache.hadoop.fs.Path;
+
+import java.io.IOException;
 
 public class hdfs_access {
+    public static void main(String[] args) throws IOException {
+
+        Configuration  configuration  =  new  Configuration();
+        configuration.set("fs.defaultFS",  "hdfs://localhost:9000");
+
+        try  {
+            FileSystem  fileSystem  =  FileSystem.get(configuration);
+            Path  path  =  new  Path("/");
+            Path[]  listOfFiles  =  fileSystem.listPaths(path);
+            for(Path  file  :  listOfFiles){
+                System.out.println(file);
+            }
+        }  catch  (IOException  e)  {
+            e.printStackTrace();
+        }
+    }
 }
